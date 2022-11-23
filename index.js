f4c83745e0c1998b9297b6d26e7879f86476da07
tony555t <tonytullah764@gmail.com>
2022-11-23 15:50:04 +0300

    phase 1

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+let mostProfitableNeighborhood = "Chelsea";
+//let  Chelsea = mostProfitableNeighborhood;
+let companyCeo = "Susan Smith";
